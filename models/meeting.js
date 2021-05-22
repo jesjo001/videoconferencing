bac12d123c232a1aa17b37376a45b2a491bfba86
jesjo001 <automationlounge@gmail.com>
2021-05-22 15:50:00 -0400

    implemented new theme

---
+++ models/meeting.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const meetingSchema = new Schema({
+    title: {
+        type: String,
+        require: true
+    },
+    briefInfo: {
+        type: String,
+        require: false
+    },
+    startAt: {
+        type: Date,
+        require: true
+    },
+    time: {
+        type: String,
+        require: true
+    },
+    userId: {
+        type: String,
+        require: true
+    },
+}, { timestamps: true });
+
+const Meeting = mongoose.model('Meeting', meetingSchema);
+module.exports = Meeting;
