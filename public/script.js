59449fa1b15c817dbc2157cce53f2914c1a0c8ec
Josh <automationlounge@gmail.com>
2021-07-16 17:01:34 +0100

    romm setting section implemented

--- public/script.js
+++ public/script.js
@@ -15,45 +15,52 @@ let myVideoStream
 const myVideo = document.createElement('video')
 myVideo.muted = true;
 const peers = {}
-navigator.mediaDevices.getUserMedia({
+let videoConstraint = {
     video: true,
-    audio: true
-}).then(stream => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream)
+    audio: {
+            sampleSize: 16,
+            channelCount: 2,
+            echoCancellation: true,
+            noiseSuppression: true
+           }
+}
+navigator.mediaDevices.getUserMedia(videoConstraint)
+    .then(stream => {
+        myVideoStream = stream;
+        addVideoStream(myVideo, myVideoStream)
 
-    peer.on('call', call => {
-        call.answer(stream)
-        const video = document.createElement('video')
+        peer.on('call', call => {
+            call.answer(stream)
+            const video = document.createElement('video')
 
-        call.on('stream', userVideoStream => {
-            addVideoStream(video, userVideoStream)
+            call.on('stream', userVideoStream => {
+                addVideoStream(video, userVideoStream)
+                attachUser(newUser)
+            })
+        })
+
+        socket.on('user-connected', (userId) => {
+            connectToNewUser(userId, myVideoStream);
             attachUser(newUser)
         })
+
+        let text = $('input')
+        $('html').keydown((e) => {
+            console.log("key down")
+            if (e.which == 13 && text.val().length !== 0) {
+                console.log(text.val())
+                socket.emit('message', text.val());
+                text.val('')
+            }
+        });
+
+
+        socket.on('createMessage', message => {
+            console.log("new message is ", message)
+            $('ul').append(`<li class="message"><b>${newUser}</b><br/>${message} </li>`)
+            scrollToButtom()
+        })
     })
-
-    socket.on('user-connected', (userId) => {
-        connectToNewUser(userId, stream);
-        attachUser(newUser)
-    })
-
-    let text = $('input')
-    $('html').keydown((e) => {
-        console.log("key down")
-        if (e.which == 13 && text.val().length !== 0) {
-            console.log(text.val())
-            socket.emit('message', text.val());
-            text.val('')
-        }
-    });
-
-
-    socket.on('createMessage', message => {
-        console.log("new message is ", message)
-        $('ul').append(`<li class="message"><b>${newUser}</b><br/>${message} </li>`)
-        scrollToButtom()
-    })
-})
 
 socket.on('user-disconnected', userId => {
     if (peers[userId]) peers[userId].close()
@@ -120,7 +127,7 @@ const muteUnmute = () => {
     }
 }
 
-//Video control section 
+//Video control section
 //Play stop video
 
 const playStop = () => {
@@ -135,11 +142,43 @@ const playStop = () => {
     }
 }
 
+const playStopOthersVideo = () => {
+    console.log("Got here ")
+    let videoTracks = myVideoStream.getVideoTracks();
+console.log("numbers of video are ", videoTracks.length);
+    let count = 0
+
+    videoTracks.forEach((track, i) => {
+      if(track.enabled && i !== 0){
+        track.enabled = false;
+        console.log("index enabled is: ", i);
+        count ++
+
+      } else if(track.disabled && i !== 0) {
+        track.enabled = true;
+        console.log("index disabled is: ", i);
+        count ++
+      }
+
+      console.log("Videos Enabled/disabled are ", count)
+
+    });
+
+    // let enabled = myVideoStream.getVideoTracks()[0].enabled;
+    // if (enabled) {
+    //     myVideoStream.getVideoTracks()[0].enabled = false;
+    //     setStopVideo()
+    // } else {
+    //     setPlayVideoIcon()
+    //     myVideoStream.getVideoTracks()[0].enabled = true;
+    // }
+}
+
 const setMuteButton = () => {
     //unmute
     const html = `
         <i class="unmute fas fa-microphone-slash"></i>
-        <span></span>        
+        <span></span>
     `
     document.querySelector('.main_mute_button').innerHTML = html;
 }
@@ -178,16 +217,15 @@ const hideChat = () => {
 
     let html = `<i class="fas fa-comment-alt" aria-hidden="true"></i>`
     let userHtml = '<i class="fas fa-user-friends" aria-hidden="true"></i>';
+    let settingIcon = '<i class="fas fa-shield-alt" aria-hidden="true"></i>';
 
     let chatWindow = document.querySelector(".main_right")
     let mainLeftWindow = document.querySelector(".main_left")
-    let mainChatWindow = document.querySelector(".main_chat_window")
-    let mainMessageContainer = document.querySelector(".main_message_container")
-    let messageInput = document.querySelector(".main_message_container input")
-
     let userWindow = document.querySelector('.main_right_users')
+    let settingsWindow = document.querySelector(".main_right_settings")
 
     if (userWindow.style.display !== 'none') userWindow.style.cssText = "flex: 0; display: none"
+    if (settingsWindow.style.display !== 'none') settingsWindow.style.cssText = "flex: 0; display: none"
 
     if (chatWindow.style.display == 'none') {
         userWindow.style.display = 'display: none'
@@ -197,39 +235,40 @@ const hideChat = () => {
         html = `<i class="fas fa-comment-alt stop"></i>`;
         document.querySelector('.comment').innerHTML = html;
         document.querySelector(".users").innerHTML = userHtml;
+        document.querySelector('.settings').innerHTML = settingIcon;
     } else {
         mainLeftWindow.style.cssText = 'flex: 1; display: flex; flex-direction: column;'
         chatWindow.style.cssText += 'flex: 0; display: none'
         document.querySelector('.comment').innerHTML = html;
-        console.log("display is not none")
+        console.log("display is not none");
     }
 }
 
 
 const toogleUsers = () => {
 
-    let html = '<i class="fas fa-user-friends" aria-hidden="true"></i>';
+    let userIconHtml = '<i class="fas fa-user-friends" aria-hidden="true"></i>';
     let chatHtml = '<i class="fas fa-comment-alt" aria-hidden="true"></i>'
+    let settingIcon = '<i class="fas fa-shield-alt" aria-hidden="true"></i>';
 
     let chatWindow = document.querySelector(".main_right")
     let mainLeftWindow = document.querySelector(".main_left")
-    let mainChatWindow = document.querySelector(".main_chat_window")
-    let mainMessageContainer = document.querySelector(".main_message_container")
-    let messageInput = document.querySelector(".main_message_container input")
-
     let userWindow = document.getElementById('userArea')
+    let settingsWindow = document.querySelector(".main_right_settings")
+
 
     if (chatWindow.style.display !== 'none') chatWindow.style.cssText = 'flex: 0; display: none'
-
+    if (settingsWindow.style.display !== 'none') settingsWindow.style.cssText = 'flex: 0; display: none'
     if (userWindow.style.display === 'none') {
         // then activate users window
         chatWindow.style.cssText = ' display: none'
         userWindow.style.cssText = 'display: flex; '
         mainLeftWindow.style.cssText = 'flex: 0.8; display: flex; flex-direction: column;'
 
-        html = `<i class="fas fa-user-friends stop"></i>`;
-        document.querySelector('.users').innerHTML = html;
+        userIconHtml = `<i class="fas fa-user-friends stop"></i>`;
+        document.querySelector('.users').innerHTML = userIconHtml;
         document.querySelector('.comment').innerHTML = chatHtml;
+        document.querySelector('.settings').innerHTML = settingIcon;
 
     } else {
 
@@ -237,13 +276,48 @@ const toogleUsers = () => {
         userWindow.style.cssText = 'flex: 0; display: none'
         mainLeftWindow.style.cssText = 'flex: 1; display: flex; flex-direction: column;'
 
-        html = `<i class="fas fa-user-friends"></i>`;
-        document.querySelector('.users').innerHTML = html;
+        userIconHtml = `<i class="fas fa-user-friends"></i>`;
+        document.querySelector('.users').innerHTML = userIconHtml;
         console.log("display is not none")
     }
 }
 
+const toggleSettings = () => {
 
+    let userIconHtml = '<i class="fas fa-user-friends" aria-hidden="true"></i>';
+    let chatHtml = '<i class="fas fa-comment-alt" aria-hidden="true"></i>'
+    let settingIcon = '<i class="fas fa-shield-alt" aria-hidden="true"></i>';
+
+    let chatWindow = document.querySelector(".main_right")
+    let mainLeftWindow = document.querySelector(".main_left")
+    let userWindow = document.querySelector('.main_right_users')
+
+    let settingsWindow = document.querySelector(".main_right_settings")
+
+    if (chatWindow.style.display !== 'none') chatWindow.style.cssText = 'flex: 0; display: none'
+    if (userWindow.style.display !== 'none') userWindow.style.cssText = "flex: 0; display: none"
+
+
+    if (settingsWindow.style.display === 'none') {
+        console.log("Activating Setting area")
+        userWindow.style.cssText = 'display: none'
+        settingsWindow.style.cssText = 'display: flex; flex:0..2 '
+        mainLeftWindow.style.cssText = 'flex: 0.8; display: flex; flex-direction: column;'
+
+        settingIcon = '<i class="fas fa-shield-alt stop" aria-hidden="true"></i>';
+        document.querySelector('.users').innerHTML = userIconHtml;
+        document.querySelector('.comment').innerHTML = chatHtml;
+        document.querySelector('.settings').innerHTML = settingIcon;
+    } else {
+        console.log()
+        mainLeftWindow.style.cssText = 'flex: 1; display: flex; flex-direction: column;'
+        settingsWindow.style.cssText = 'flex: 0; display: none; '
+
+        document.querySelector('.settings').innerHTML = settingIcon;
+    }
+}
+
+// Modal for username upon entering chat
 if (newUser === "user") {
     console.log("user needs a dialog")
 
@@ -252,4 +326,59 @@ if (newUser === "user") {
         show: true,
         backdrop: 'static'
     })
+}
+
+const setSupportedConstraints = () => {
+    let constraintList = document.getElementById("sendVideoResolution");
+    let supportedConstraints = navigator.mediaDevices.getSupportedConstraints();
+
+    for (let constraint in supportedConstraints) {
+        if (supportedConstraints.hasOwnProperty(constraint)) {
+            let elem = document.createElement("option", { value: constraint });
+            const newContent = document.createTextNode(`${constraint}`);
+            elem.appendChild(newContent);
+            constraintList.appendChild(elem);
+        }
+    }
+
+}
+
+
+const changeVideoSettings = () => {
+    const senderVideoSelect = document.getElementById('sendVideoResolution');
+    const receiveVideoSelect = document.getElementById('receiveVideoRes')
+
+    //get form value
+    const selectedValue = senderVideoSelect.value
+    //get users video track
+    const videoTracks = myVideoStream.getVideoTracks()[0];
+    //set constraints for user video with selected value
+    let videoConstraint = { "height": parseInt(selectedValue) }
+    //apply Constraint to video track
+    setConstraints(videoTracks, videoConstraint)
+}
+
+
+const setConstraints = (videoTracks, videoConstraint) => {
+
+    videoTracks.applyConstraints(videoConstraint)
+        .then(() => {
+            // Do something with the track such as using the Image Capture API.
+            console.log("applied")
+            console.log(videoTracks.getSettings())
+        })
+        .catch(e => {
+            // The constraints could not be satisfied by the available devices.
+            console.log("Error", e)
+        });
+}
+
+
+const stopUsersVideo = () => {
+  const userChoice = document.getElementById('usersVideoToggle')
+  console.log("users choice: ", userChoice.checked)
+  if(userChoice.checked){
+    playStopOthersVideo()
+  }
+  // playStopOthersVideo()
 }
